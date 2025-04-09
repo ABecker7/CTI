5ad0ab0fe109569a984468721ff4e8e2c645da93
ABecker7 <andybecker300@gmail.com>
2025-04-09 13:16:03 -0400

    virustotal enrichment

--- app/cti.py
+++ app/cti.py
@@ -3,6 +3,7 @@ import json
 from datetime import datetime
 from dotenv import load_dotenv
 import os
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
 load_dotenv()
@@ -10,12 +11,38 @@ load_dotenv()
 
 
 api_key = os.getenv("OTX_API_KEY")
-
+vt_api_key = os.getenv("VT_API_KEY")
+vt_url = 'https://www.virustotal.com/api/v3/'
 OTX_API_KEY = api_key
+VT_API_KEY = vt_api_key
 OTX_URL = 'https://otx.alienvault.com/api/v1/indicators/export?type=IPv4&Limit=50'
 IOC_FILE_PATH = '/var/ossec/etc/lists/malicious_ips.txt'
 RULES_FILE_PATH = '/var/ossec/etc/rules/local_rules.xml'
 
+class Threat(SQLModel, table=True):
+
+    id: int | None = Field(default=None, primary_key=True)
+    indicator: str | None = None
+    type: str | None = None
+    confidence: int | None = None
+    cve: str | None = None
+    attack_pattern: str | None = None
+    timestamp: str | None = None
+
+
+# class ThreatEnrichment(SQLModel, table=True):
+
+
+
+sqlite_file_name = "cti.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(ngine)
 
 
 def fetch_otx_data():
@@ -24,12 +51,40 @@ def fetch_otx_data():
     }
     response = requests.get(OTX_URL, headers=headers)
     if response.status_code == 200:
-        print('test')
-        return response.text.split('\n')
+    
+        json_data = json.loads(response.text)
+    
+        for i in json_data['results']:
+            i = Threat(id=i['id'], indicator=i['indicator'], type=i['type']) 
+            with Session(engine) as session:
+                session.add(i)
+                session.commit()
     else:
         print("failed to fetch data:", response.status_code)
         return []
+    
+def select_threats():
+    headers = {
+        "accept":"application/json",
+        "x-apikey":vt_api_key,
+    }
 
+    with Session(engine) as session:
+        statement = select(Threat)
+        results = session.exec(statement)
+        for threat in results:
+
+            if threat.type == "IPv4":
+                vt_url = f"https://www.virustotal.com/api/v3/ip_addresses/{threat.indicator}"
+                response = requests.get(vt_url, headers=headers)
+                json_data = json.loads(response.text)
+                for i in json_data:
+                    print(i)
+
+
+
+
+                 
 
 def write_iocs_to_wazuh_file(iocs):
     with open(IOC_FILE_PATH, 'w') as f:
@@ -39,5 +94,10 @@ def write_iocs_to_wazuh_file(iocs):
     print(f"[+] {len(iocs)} IOCs written to Wazuh list.")
 
 
+def main():
+    create_db_and_tables()    
+    fetch_otx_data()
+    
+
 if __name__ == "__main__":
-    print(fetch_otx_data())   
+    select_threats()
--- app/db.py
+++ app/db.py
@@ -2,9 +2,10 @@ from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
 class Threat(SQLModel, table=True):
-    ioc_type: str | None = Field(default=None, primary_key=True)
-    value: str | None = None
-    source: str | None = None
+    
+    id: int | None = Field(default=None, primary_key=True)
+    indicator: str | None = None
+    type: str | None = None
     confidence: int | None = None
     cve: str | None = None
     attack_pattern: str | None = None
@@ -19,19 +20,17 @@ engine = create_engine(sqlite_url, echo=True)
 def create_db_and_tables():  
     SQLModel.metadata.create_all(engine)
 
-def create_threats():
-    threat1 = Threat(ioc_type="ip", value="100.100.100.100",source="test")
-    threat2 = Threat(ioc_type="domain", value="evil.com",source="eviltest")
+def create_threats(id_id, indicator_ind):
+    threat1 = Threat(id=id_id, indicator=indicator_ind)
     
     with Session(engine) as session:
         session.add(threat1)
-        session.add(threat2)
 
         session.commit()
 
 def select_threats():
     with Session(engine) as session:
-        statement = select(Threat).where(Threat.ioc_type == "domain")
+        statement = select(Threat).where(Threat.type == "domain")
         results = session.exec(statement)
         for threat in results:
 
@@ -45,4 +44,4 @@ def main():
     select_threats()
 
 if __name__ == "__main__":
-    main()
+    select_threats()
